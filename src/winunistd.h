f6d10bb32cfb7900f8e0f669eadb74383528be38
szy <git@szy.lol>
2023-07-06 22:53:27 +0200

    add wincompat for sleep(3)

--- src/winunistd.h
+++ src/winunistd.h
@@ -6,6 +6,7 @@
 #include <direct.h>
 #include <io.h>
 #include <conio.h>
+#include <synchapi.h>
 #define F_OK 0
 #define strdup _strdup
 #define strtok_r strtok_s
@@ -14,6 +15,7 @@
 #define getcwd _getcwd
 #define unlink _unlink
 #define mkdir(__p, __ignored) _mkdir(__p)
+#define sleep(__n) Sleep((__n)*1000)
 
 #else
 #include <unistd.h>
